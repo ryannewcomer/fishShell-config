29c26dca3561c023cfb8091a1c7930ce3028b4fd
Ryan Hung <ryanhungryan@gmail.com>
2024-10-05 12:20:53 +0100

    update fish config

--- config.fish
+++ config.fish
@@ -1,3 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+set -gx EDITOR (which nvim)
+zoxide init fish | source
+starship init fish | source
+alias l="eza --color=always --icons --group-directories-first --all --long"
+alias sf="source .config/fish/config.fish"
+
+abbr c clear
+abbr rm 'rm -rf'
+abbr ps 'paru -S'
+abbr p 'paru'
