58e14f02f4daea81199e4213c7addf7fc1f12d57
Ryan Hung <ryanhungryan@gmail.com>
2024-11-11 21:39:01 +0000

    feat: add more alias for git
    add a alias for git status, git add and commit

--- config.fish
+++ config.fish
@@ -7,15 +7,15 @@ zoxide init fish | source
 alias  l "eza --long --color --icons --all " 
 alias cat "bat"
 alias c "clear"
-
+alias gs "git status"
+alias ga "git add ."
+alias gc "czg"
+alias gp "git pull"
 
 # path
 set -gx EDITOR nvim
 fish_add_path -g /home/ryanu/.cargo/bin
 
-abbr g git
 abbr rm 'rm -rf'
-abbr gs 'git status'
-abbr gp 'git push'
 
 alias unimatrix /usr/local/bin/unimatrix
